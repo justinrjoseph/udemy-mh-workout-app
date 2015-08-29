f1c9bbaf0178f2fdc1d7d7c9ea56d853cb82aa76
Justin Joseph <justin.joseph0@yahoo.com>
2015-08-29 13:12:45 +0000

    Add homepage spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Homepage" do
+  
+  scenario "A user visit the homepage" do
+    visit "/"
+    
+    expect(page).to have_link "Athletes Den"
+    expect(page).to have_link "Home"
+    expect(page).to have_content "Workout Lounge"
+  end
+  
+end

2ea2b4dbb8c58cb8df4fdd58b9263bbb2b9b325b
Justin Joseph <justin.joseph0@yahoo.com>
2015-09-02 00:37:04 +0000

    Clean workout model validation code

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,8 +1,7 @@
 class Workout < ActiveRecord::Base
   belongs_to :user
   
-  validates :duration, presence: true
-  validates :duration, numericality: { only_integer: true }
+  validates :duration, presence: true, numericality: { only_integer: true }
   validates :details, presence: true
   validates :date, presence: true
   validates :user_id, presence: true
